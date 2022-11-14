c01ad2e14fa646b32febd685a7c956b991126b68
Sanjay S Gangwar <107429646+SanjaySinghGangwar@users.noreply.github.com>
2022-11-14 18:33:38 +0530

    splash screen fixed

--- app/src/main/java/com/szip/smartdream/Controller/WelcomeActivity.java
+++ app/src/main/java/com/szip/smartdream/Controller/WelcomeActivity.java
@@ -1,43 +1,35 @@
 package com.szip.smartdream.Controller;
 
-import android.Manifest;
+import static com.szip.smartdream.MyApplication.FILE;
+import static com.szip.smartdream.Util.mutils.checkALlPermissions;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.WindowManager;
 
-import com.szip.smartdream.Intro;
 import com.szip.smartdream.MyApplication;
 import com.szip.smartdream.R;
 import com.szip.smartdream.Service.BleService;
 import com.szip.smartdream.Util.MathUitl;
 import com.szip.smartdream.View.MyAlerDialog;
 
-import static com.szip.smartdream.MyApplication.FILE;
+public class WelcomeActivity extends BaseActivity  {
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class WelcomeActivity extends BaseActivity implements Runnable {
-
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
     /**
      * 延时线程
      */
     private Thread thread;
     private int time = 3;
     private int SleepEECode = 100;
-    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
-
     /**
      * 轻量级文件
      */
@@ -46,6 +38,7 @@ public class WelcomeActivity extends BaseActivity implements Runnable {
     private boolean isFirst;
     private MyApplication app;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,65 +47,58 @@ public class WelcomeActivity extends BaseActivity implements Runnable {
 
 
 
-        app = (MyApplication)getApplicationContext();
-        if(sharedPreferencesp == null){
-            sharedPreferencesp = getSharedPreferences(FILE,MODE_PRIVATE);
+
+        app = (MyApplication) getApplicationContext();
+        if (sharedPreferencesp == null) {
+            sharedPreferencesp = getSharedPreferences(FILE, MODE_PRIVATE);
         }
 
-        isFirst = sharedPreferencesp.getBoolean("isFirst",true);
+        isFirst = sharedPreferencesp.getBoolean("isFirst", true);
         app.setUserInfo(MathUitl.loadInfoData(sharedPreferencesp));
 
-        if(isFirst){
+        if (isFirst) {
             MyAlerDialog.getSingle().showAlerDialogWithPrivacy(false
-                     , new MyAlerDialog.AlerDialogOnclickListener() {
-                        @Override
-                        public void onDialogTouch(boolean flag) {
-                            if (flag){
-                                sharedPreferencesp.edit().putBoolean("isFirst",false).commit();
-                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                                    if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED
-                                            || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
-                                            || checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
-                                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                                                Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, SleepEECode);
-                                    }
-                                    initData();
-                                }else {
-                                    initData();
-                                }
-                            }else{
-                                finish();
+                    , flag -> {
+                        if (flag) {
+                            sharedPreferencesp.edit().putBoolean("isFirst", false).commit();
+                            if (checkALlPermissions(WelcomeActivity.this)) {
+                                init();
+                            } else {
+                                startActivity(
+                                        new Intent(
+                                                android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                                Uri.fromParts("package", getPackageName(), null)
+                                        )
+                                );
                             }
+                        } else {
+                            finish();
                         }
-                    },this);
-        }else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED
-                        || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
-                        || checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
-                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, SleepEECode);
-                }
-                initData();
-            }else {
-                initData();
-            }
+                    }, this);
+        } else {
+            init();
         }
- }
+    }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkALlPermissions(this);
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == SleepEECode){
+        if (requestCode == SleepEECode) {
             int code = grantResults[0];
             int code1 = grantResults[1];
             int code2 = grantResults[2];
-            if (code == PackageManager.PERMISSION_GRANTED&&code1 == PackageManager.PERMISSION_GRANTED&&code2 == PackageManager.PERMISSION_GRANTED){
-                initData();
+            if (code == PackageManager.PERMISSION_GRANTED && code1 == PackageManager.PERMISSION_GRANTED && code2 == PackageManager.PERMISSION_GRANTED) {
+                //initData();
                 init();
-            }else {
-               // WelcomeActivity.this.finish();
+            } else {
+                // WelcomeActivity.this.finish();
 
                 //When user Deny the Permissions User Send to the Settings And Permission Ui of Mobile
 
@@ -128,23 +114,22 @@ public class WelcomeActivity extends BaseActivity implements Runnable {
     }
 
 
-
-    private void initData() {
-        thread = new Thread(this);
-        thread.start();
-    }
-
-        public void run() {
-        try {
-            while (time != 0){
-                Thread.sleep(1000);
-                time = time -1;
-            }
-            init();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+//    private void initData() {
+//        thread = new Thread(this);
+//        thread.start();
+//    }
+//
+//    public void run() {
+//        try {
+//            while (time != 0) {
+//                Thread.sleep(1000);
+//                time = time - 1;
+//            }
+//            init();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 
     @Override
@@ -152,30 +137,30 @@ public class WelcomeActivity extends BaseActivity implements Runnable {
         return super.onCreateView(name, context, attrs);
 
 
-
     }
 
-    private void init(){
-        if (app.getStartState() == 0){//已登录
-            if (app.getUserInfo().getDeviceCode()!=null){//已绑定
+    private void init() {
+
+        if (app.getStartState() == 0) {//已登录
+            if (app.getUserInfo().getDeviceCode() != null) {//已绑定
                 BleService.getInstance().setmMac(app.getUserInfo().getDeviceCode());
                 BleService.getInstance().startConnectDevice();
                 Intent guiIntent = new Intent();
                 guiIntent.setClass(WelcomeActivity.this, MainActivity.class);
                 startActivity(guiIntent);
                 finish();
-            }else {//未绑定
+            } else {//未绑定
                 Intent in = new Intent();
                 in.setClass(WelcomeActivity.this, FindDeviceActivity.class);
                 startActivity(in);
                 finish();
             }
-        }else if (app.getStartState() == 1){//未登录
+        } else if (app.getStartState() == 1) {//未登录
             Intent in = new Intent();
             in.setClass(WelcomeActivity.this, LoginActivity.class);
             startActivity(in);
             finish();
-        }else {//登陆过期
+        } else {//登陆过期
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
--- app/src/main/java/com/szip/smartdream/Util/mutils.java
+++ app/src/main/java/com/szip/smartdream/Util/mutils.java
@@ -1,22 +1,15 @@
 package com.szip.smartdream.Util;
 
-import static android.support.v4.app.ActivityCompat.requestPermissions;
-import static android.support.v4.content.PermissionChecker.PERMISSION_GRANTED;
-import static android.support.v4.content.PermissionChecker.checkSelfPermission;
-
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.support.annotation.RequiresApi;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.szip.smartdream.Controller.MainActivity;
-import com.szip.smartdream.Controller.WelcomeActivity;
 
 public class mutils {
     public static void mLog(String message) {
@@ -41,19 +34,20 @@ public class mutils {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 
-//    public static Boolean checkALlPermissions(){
-//
-//        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED
-//                || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
-//                || checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-//            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-//                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE} , );
-//
-//                return false;
-//            }
-//            return true;
-//
-//    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public static Boolean checkALlPermissions(Activity context) {
+
+        if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED
+                || context.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+                || context.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            context.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+
+            return false;
+        }
+        return true;
+
+    }
 
 
 }
